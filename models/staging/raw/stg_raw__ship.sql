32bde99aa896c8dc9debe31010562b51bf9196a7
riana mamy <rianahmamy@gmail.com>
2024-11-07 18:21:26 +0000

    Create staging model

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,8 +11,9 @@ renamed as (
     select
         orders_id,
         shipping_fee,
+        shipping_fee_1,
         logcost,
-        CAST(ship_cost AS INT64) AS ship_cost
+        ship_cost
 
     from source
 
